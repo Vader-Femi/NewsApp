749754bd7c8e5460e7da6115e7ce49ea5d046959
Femi <oluwafemi.za@gmail.com>
2024-10-06 22:13:14 +0100

    Deleted unusd dependency

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:myapplication/configs/theme/AppTheme.dart';
 import 'package:myapplication/features/choose_mode/presentation/bloc/theme_cubit.dart';
-import 'package:myapplication/features/daily_news/presentation/pages/home/daily_news.dart';
 import 'package:myapplication/features/splash/presentation/pages/splash.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:myapplication/service_locator.dart';
